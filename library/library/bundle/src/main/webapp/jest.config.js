2b5a6e0672ae2a0eca9f75b067383d680d00e5bb
Volkov Denis <80678154+VolkovDenisS@users.noreply.github.com>
2025-09-03 16:48:42 +0300

    Delete library/library directory

--- library/library/bundle/src/main/webapp/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/shell/',
-    '<rootDir>/libs/**/',
-  ],
-};
--- library/library/bundle/src/main/webapp/postinstall.js
+++
@@ -1,3 +0,0 @@
-const execSync = require('child_process').execSync;
-
-execSync('schematics @helix/schematics:workspace', { stdio: 'inherit' });
